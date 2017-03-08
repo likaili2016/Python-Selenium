ffa918d027370a8ebcaeb92a36134ddfbf8ae69b
likaili <likaili@renrenche.com>
2017-03-08 16:25:34 +0800

    提交显示等待与隐式等待的case

--- WebUI/TestCase/test6.py
+++ WebUI/TestCase/test6.py
@@ -7,7 +7,18 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 driver.get("http://www.baidu.com")
 
-element = WebDriverWait(driver, 5, 0.5).until(EC.presence_of_element_located(By.ID, "kw"))
-driver.find_element_by_id("kw").send_keys('selenium')
+element = WebDriverWait(driver, 5, 0.5, None).until(EC.presence_of_element_located(By.ID, 'kw'), u'元素可见')
+
+element.send_keys('selenium')
 driver.quit()
 
+
+'''expected_conditions提供了其他的预期条件实现的方法：
+   title_is 判断标题是否**
+   title_contains 判断标题是否包含**
+   presence_of_element_located 判断元素是否存在
+   visible_of_element_located 判断元素是否可见
+   visible_of 是否可见
+   presence_of_all_element_located 判断一组元素是否可见
+   text_to_be_present_in_element 判断元素是否有xx文本信息
+   '''
